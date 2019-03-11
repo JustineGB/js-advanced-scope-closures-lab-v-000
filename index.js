bb8a04bb6968b0fa6f8c8a65f54da338a76bd38e
Justine B <justinegayton@gmail.com>
2019-03-11 16:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ let driverId = 0
 function produceDrivingRange(){
   return function(first, second){
     let blockRange = second - first
-    if blockRange > 10;
-    return  
-    else blockRange < 10 ;
+    if (blockRange > 10);
+    return 
+    if (blockRange < 10);
     return 
   }
 }
+
+
+('2 blocks out of range')
+      expect(eightBlockRange('10th', '14th')).to.equal('within range by 4')
+
 
 function produceTipCalculator(){
   
