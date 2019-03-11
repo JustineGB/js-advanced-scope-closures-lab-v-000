8d2459edf2900390d3c961cef8685d572a386722
Justine B <justinegayton@gmail.com>
2019-03-11 16:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let driverId = 0
 
 function produceDrivingRange(blockRange){
   return function(first, second){
-    let distance = Math.abs
+    let distance = Math.abs(first - second)
     let blockRange = second - first
     if (blockRange > 10);
     return `${blockRange} blocks out of range`;
