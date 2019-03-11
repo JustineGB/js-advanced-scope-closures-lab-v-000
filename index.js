96a5e78404e61381fccabcd54d57a1983be2b1a3
Justine B <justinegayton@gmail.com>
2019-03-11 17:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let driverId = 0 
 
-function produceDrivingRange(blockRange){
-  return function(firstNum, secondNum){
-    let x = parseInt(firstNum);
-    let y = parseInt(secondNum);
-    let distance = Math.abs(y - x);
-    let difference = Math.abs(distance - blockRange);
-    if (distance > blockRange) {
+function produceDrivingRange(blockRange){ //12
+  return function(firstNum, secondNum){ //10th, 12th 
+    let x = parseInt(firstNum); //10 
+    let y = parseInt(secondNum); //12
+    let distance = Math.abs(y - x); //2
+    let difference = Math.abs(distance - blockRange); //10 
+    if (distance > blockRange) { //2 > 12?
       return `${difference} blocks out of range`;}
       else 
       {return `within range by ${difference}`;}
