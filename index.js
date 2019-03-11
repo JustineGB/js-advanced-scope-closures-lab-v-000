cd27d38f20e76d8d4c03bb1940d7d0faa9708ec1
Justine B <justinegayton@gmail.com>
2019-03-11 16:37:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let driverId = 0 
 
 function produceDrivingRange(){
-  return function(first, second){}
-  blockRange
+  return function(first, second){
+    let blockRange = second - first
+    if blockRange > 10;
+    return 
+    else blockRange < 10 ;
+    return 
+ 
+  }
 }
 
 function produceTipCalculator(){
